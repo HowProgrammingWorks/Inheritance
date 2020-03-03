f1210cfbc85c315aeacc2784ac397f3d9fba92c8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-03 23:40:44 +0200

    Change method to getter

--- JavaScript/4-lambda.js
+++ JavaScript/4-lambda.js
@@ -22,7 +22,7 @@ const serializable = Category => class extends Category {
 };
 
 const measurable = Category => class extends Category {
-  area() {
+  get area() {
     return this.width * this.height;
   }
 };
@@ -51,7 +51,6 @@ const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
 // Usage
 
-
 const Square1 = equilateral(serializable(measurable(movable(scalable(Rect)))));
 const toSquare = pipe(equilateral, serializable, measurable, movable, scalable);
 const Square2 = toSquare(Rect);
@@ -60,10 +59,10 @@ const p1 = new Square1(10, 20, 50);
 p1.scale(1.2);
 p1.move(10, 7);
 console.log(p1.toString());
-console.log('Area:', p1.area());
+console.log('Area:', p1.area);
 
 const p2 = new Square2(10, 20, 50);
 p2.scale(1.2);
 p2.move(-10, 5);
 console.log(p2.toString());
-console.log('Area:', p2.area());
+console.log('Area:', p2.area);
